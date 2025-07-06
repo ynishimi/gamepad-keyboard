810995e6e79ed63d82fabb983aa15213ff373e82
ynishimi <nishimi.yuki.37z@st.kyoto-u.ac.jp>
2025-07-07 00:23:54 +0900

    change evaluate methods

--- evaluate.py
+++ evaluate.py
@@ -5,19 +5,29 @@ answer = open('answer.txt')
 
 count_correct = 0
 count_wrong = 0
-wrong_dict: dict[str, list[str]] = dict()
+count_unk = 0
+unk_dict: dict[str, int] = dict()
+wrong_dict: dict[str, dict[str, int]] = dict()
 
 for res_line, ans_line in zip(result, answer):
     for res_word, ans_word in zip(res_line.split(), ans_line.split()):
-        if res_word == ans_word:
+        if 'UNK' in res_word:
+            count_unk += 1
+            unk_dict[ans_word] = unk_dict.get(ans_word, 0) + 1
+        elif res_word == ans_word:
             count_correct += 1
         else:
             count_wrong += 1
-            wrong_dict.setdefault(ans_word, list()).append(res_word)
+            wrong_dict_element =  wrong_dict.setdefault(ans_word, dict())
+            wrong_dict_element[res_word] = wrong_dict_element.get(res_word, 0) + 1
 
-accuracy = format(count_correct / (count_correct + count_wrong) * 100, '.2f')
-print(f'correct: {count_correct}, wrong: {count_wrong}')
+accuracy = format(count_correct / (count_correct + count_wrong + count_unk) * 100, '.2f')
+print(f'correct: {count_correct}, wrong: {count_wrong}, unknown: {count_unk}')
 print(f'accuracy: {accuracy} %')
+
+unk_dict_sorted = sorted(unk_dict.items(), key=lambda x: x[1], reverse=True)
 
 with open('wrong_word_list.json', 'w') as f:
     json.dump(wrong_dict, f, indent=2)
+with open('unk_word_list.json', 'w') as f:
+    json.dump(unk_dict_sorted, f, indent=2)
